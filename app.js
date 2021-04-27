6f31121759779edd152d2355ee5cf7f7378037e6
maxim <85100026@study.bsuir.by>
2021-04-27 14:15:12 +0300

    fix: implement search using batch request

--- app.js
+++ app.js
@@ -54,43 +54,31 @@ class App {
     };
   }
 
-  //
-  // async trace(results, func) {
-  //   let array = [];
-  //   for (let i = 0; i < results.length; ++i) {
-  //     const result = results[i]
-  //     array.push(await func(result))
-  //   }
-  //   return array
-  // }
-  //
-
   async search(query) {
     try {
-      // const iexSearchQueryResults = await this.iex.search(query)
-      // const results = await this.trace(iexSearchQueryResults, async resultItem => {
-      //   const symbol = resultItem.symbol
-      //   const price = await this.lookup(symbol)
-      //   return {
-      //     symbol: symbol,
-      //     name: resultItem.securityName,
-      //     price: isNaN(price) ? "Unknown" : price.toFixed(2),
-      //     url: `/stock/${symbol}`
-      //   }
-      // })
       const iexSearchQueryResults = await this.iex.search(query);
-      const results = await Promise.all(
-        iexSearchQueryResults.map(async (resultItem) => {
-          const symbol = resultItem.symbol;
-          const price = await this.lookup(symbol);
-          return {
-            symbol: symbol,
-            name: resultItem.securityName,
-            price: isNaN(price) ? "Unknown" : price.toFixed(2),
-            url: `/stock/${symbol}`,
-          };
-        })
-      );
+      const symbols = iexSearchQueryResults
+        .map((resultItem) => resultItem.symbol)
+        .join(",");
+
+      const batchResults = await this.iex
+        .batchSymbols(symbols)
+        .batch()
+        .price()
+        // .company()
+        .range("1m", iexSearchQueryResults.length);
+      const results = iexSearchQueryResults.map((resultItem) => {
+        const symbol = resultItem.symbol;
+        const price = batchResults[symbol].price;
+        const name = resultItem.securityName;
+        // const name = batchResults[symbol].company.companyName;  // contains name not for all companies
+        return {
+          symbol: symbol,
+          name: name,
+          price: Number.isFinite(price) ? price.toFixed(2) : "Unknown",
+          url: `/stock/${symbol}`,
+        };
+      });
       return {
         count: results.length,
         results: results,
